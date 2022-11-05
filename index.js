10bdc92eee0a808ac09b7838ec8288effb95b1e2
Dylan Barratt <dylanbarratt03@gmail.com>
2022-11-05 20:21:15 +0000

    added reading from a file

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
-var nodemailer = require("nodemailer");
-
-console.log(process.env.USER_EMAIL);
+const nodemailer = require("nodemailer");
+const fs = require("fs");
 
 var transporter = nodemailer.createTransport({
     service: "gmail",
@@ -11,17 +10,22 @@ var transporter = nodemailer.createTransport({
     },
 });
 
-var mailOptions = {
-    from: process.env.USER_EMAIL,
-    to: "dylanbarratt03@gmail.com",
-    subject: "Sending Email using Node.js",
-    text: "That was easy!",
-};
+const emails = fs.readFileSync("emails.txt", { encoding: "utf8", flag: "r" });
+const emailsArray = emails.split("\n");
 
-transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("Email sent: " + info.response);
-    }
+emailsArray.forEach((ele) => {
+    var mailOptions = {
+        from: process.env.USER_EMAIL,
+        to: ele,
+        subject: "Sending Email using Node.js",
+        text: "Hello " + ele,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Email sent: " + info.response);
+        }
+    });
 });
